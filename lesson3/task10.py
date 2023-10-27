ec64b81824c3a73376296a0fb87ff1638cc574a6
Mikael Malania <m.malania@noxtton.com>
2023-10-27 22:04:52 +0400

    updated required fileds

--- lesson3/task10.py
+++ lesson3/task10.py
@@ -1,12 +1,12 @@
 # yeild functions in python
 
-def simple_generator():
+def simple_easy_generator():
     yield "1"
     yield "hello"
     yield "there"
 
 
-generator = simple_generator()
+generator = simple_easy_generator()
 print(generator)
 for value in generator:
     print(value)
